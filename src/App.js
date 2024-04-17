a2c395c24a8ae09d6fc490104c120755c82c906e
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-04-17 13:12:59 +0000

    Linked sign-in and sign-up buttons to respective pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
 import Signup from './signup/signup.js';
-    import SignIn from './sign-in/sign-in-page';
+import SignIn from './sign-in/sign-in-page';
 import AdminDash from './admin-dash.js';
 import { Route, Routes, BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-   <div>
-    <BrowserRouter>
+    <div>
+      <BrowserRouter>
         <Routes>
-          {/* <Route path='/' element={<Homepage />} /> */}
-          <Route path='/signin' element={<SignIn/>} />
-          <Route path='/signup' element={<Signup/>} />
-          <Route path='/dashboard' element={ <AdminDash/>} />
-          
+          {/*<Route path='/' element={<Homepage />} /> */}
+          <Route path='/signin' element={<SignIn />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/dashboard' element={<AdminDash />} />
         </Routes>
-   {/* <Signup/> */}
-        {/* <SignIn/> */}
-      
-        </BrowserRouter>
-   </div>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/signup/signup.js
+++ src/signup/signup.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Signup = () => {
   return (
@@ -115,17 +116,14 @@ const Signup = () => {
           </form>
           <div class="flex flex-col mt-4 items-center justify-center text-sm">
             <h3>
-              <span class="cursor-default dark:text-gray-300">Have an account?</span>
-              <a
-                class="group text-blue-400 transition-all duration-100 ease-in-out"
-                href="#"
-              >
-                <span
-                  class="bg-left-bottom ml-1 bg-gradient-to-r from-blue-400 to-blue-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out"
-                >
-                  Sign In
+            <span className="cursor-default dark:text-gray-300">
+                  Don't have an account?{' '}
+                  <Link to="/signin" className="group text-blue-400 transition-all duration-100 ease-in-out">
+                    <span className="bg-left-bottom ml-1 bg-gradient-to-r from-blue-400 to-blue-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
+                      Sign In
+                    </span>
+                  </Link>
                 </span>
-              </a>
             </h3>
           </div>
 
