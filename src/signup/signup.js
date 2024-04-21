00bb922b30843499cf0de98a892454512c64e523
DzidediSenaya <dzidedisenaya@gmail.com>
2024-04-21 19:13:48 +0000

    reversed sign in form styling

--- src/signup/signup.js
+++ src/signup/signup.js
@@ -168,7 +168,7 @@ const Signup = () => {
               <h3>
                 <span className="cursor-default dark:text-gray-300">
                   Already have an account?{' '}
-                  <Link to="/signin" className="group text-blue-400 transition-all duration-100 ease-in-out">
+                  <Link to="/" className="group text-blue-400 transition-all duration-100 ease-in-out">
                     <span className="bg-left-bottom ml-1 bg-gradient-to-r from-blue-400 to-blue-400 bg-[length:0%_2px] bg-no-repeat group-hover:bg-[length:100%_2px] transition-all duration-500 ease-out">
                       Sign In
                     </span>
