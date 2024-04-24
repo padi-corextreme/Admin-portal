99569293be2e495d39e797ebce6e32a46e892437
Dzidedi Senaya <dzidedisenaya@gmail.com>
2024-04-24 16:08:55 +0000

    minor changes

--- src/promos/productcard.js
+++ src/promos/productcard.js
@@ -1,11 +1,11 @@
-const ProductCard = ({ imageUrl, title, originalPrice, discountPrice }) => (
+const ProductCard = ({ imageUrl, title,discountType , discountAmount }) => (
   <div className="w-72 bg-white shadow-md rounded-xl duration-500 hover:scale-105 hover:shadow-xl">
     <img src={imageUrl} alt="Product" className="h-80 w-72 object-cover rounded-t-xl" />
     <div className="px-4 py-3 w-72">
       <p className="text-lg font-bold text-black truncate block capitalize">{title}</p>
       <div className="flex items-center">
-        <del className="text-gray-500 mr-2">${originalPrice}</del>
-        <p className="text-lg font-semibold text-black cursor-auto my-3">${discountPrice}</p>
+        <del className="text-gray-500 mr-2">${discountAmount}</del>
+        <p className="text-lg font-semibold text-black cursor-auto my-3">${discountType}</p>
       </div>
     </div>
   </div>
